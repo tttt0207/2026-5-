b6070159c02a48d3304ed0e3002590b33fe90d9b
tttt0207 <2954328492@qq.com>
2026-05-25 21:14:04 +0800

    Add files via upload

---
+++ uart_test.py
@@ -0,0 +1,14 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyS2', 115200, timeout=1)
+
+while True:
+    # dx = 0.1cm -> 10 -> 00 0A
+    # dy = 0.2cm -> 20 -> 00 14
+    data = bytes([0x01, 0x00, 0x0A, 0x00, 0x14, 0x05])
+
+    ser.write(data)
+    print("send:", data.hex(" "))
+
+    time.sleep(0.5)
